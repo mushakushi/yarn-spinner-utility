5c469a94901c0dc809864b67033720441e2f10aa
Matthew Brown <mushakushi@gmail.com>
2024-10-23 12:41:09 -0400

    upgraded unity version and added ability to use a non generic action command handler

--- Assets/YarnSpinnerUtility/Runtime/Commands/YarnCommandDispatcher.cs
+++ Assets/YarnSpinnerUtility/Runtime/Commands/YarnCommandDispatcher.cs
@@ -62,52 +62,55 @@ namespace YarnSpinnerUtility.Runtime.Commands
         /// <param name="commandHandler">The command that will be called.</param>
         public void AddCommandHandler(string commandName, Delegate commandHandler) => commandDispatcher.AddCommandHandler(commandName, commandHandler);
         
-        /// <inheritdoc cref="AddCommandHandler"/>
+        /// <inheritdoc cref="AddCommandHandler(string, Delegate)"/>
+        public void AddCommandHandler(string commandName, Action commandHandler) => commandDispatcher.AddCommandHandler(commandName, commandHandler);
+        
+        /// <inheritdoc cref="AddCommandHandler(string, Delegate)"/>
         public void AddCommandHandler<T>(string commandName, Action<T> commandHandler) => commandDispatcher.AddCommandHandler(commandName, commandHandler);
 
-        /// <inheritdoc cref="AddCommandHandler"/>
+        /// <inheritdoc cref="AddCommandHandler(string, Delegate)"/>
         public void AddCommandHandler<T1, T2>(string commandName, Action<T1, T2> commandHandler) => commandDispatcher.AddCommandHandler(commandName, commandHandler);
 
-        /// <inheritdoc cref="AddCommandHandler"/>
+        /// <inheritdoc cref="AddCommandHandler(string, Delegate)"/>
         public void AddCommandHandler<T1, T2, T3>(string commandName, Action<T1, T2, T3> commandHandler) => commandDispatcher.AddCommandHandler(commandName, commandHandler);
 
-        /// <inheritdoc cref="AddCommandHandler"/>
+        /// <inheritdoc cref="AddCommandHandler(string, Delegate)"/>
         public void AddCommandHandler<T1, T2, T3, T4>(string commandName, Action<T1, T2, T3, T4> commandHandler) => commandDispatcher.AddCommandHandler(commandName, commandHandler);
 
-        /// <inheritdoc cref="AddCommandHandler"/>
+        /// <inheritdoc cref="AddCommandHandler(string, Delegate)"/>
         public void AddCommandHandler<T1, T2, T3, T4, T5>(string commandName, Action<T1, T2, T3, T4, T5> commandHandler) => commandDispatcher.AddCommandHandler(commandName, commandHandler);
 
-        /// <inheritdoc cref="AddCommandHandler"/>
+        /// <inheritdoc cref="AddCommandHandler(string, Delegate)"/>
         public void AddCommandHandler<T1, T2, T3, T4, T5, T6>(string commandName, Action<T1, T2, T3, T4, T5, T6> commandHandler) => commandDispatcher.AddCommandHandler(commandName, commandHandler);
 
-        /// <inheritdoc cref="AddCommandHandler"/>
+        /// <inheritdoc cref="AddCommandHandler(string, Delegate)"/>
         public void AddCommandHandler<T1, T2, T3, T4, T5, T6, T7>(string commandName, Action<T1, T2, T3, T4, T5, T6, T7> commandHandler) => commandDispatcher.AddCommandHandler(commandName, commandHandler);
 
-        /// <inheritdoc cref="AddCommandHandler"/>
+        /// <inheritdoc cref="AddCommandHandler(string, Delegate)"/>
         public void AddCommandHandler<T1, T2, T3, T4, T5, T6, T7, T8>(string commandName, Action<T1, T2, T3, T4, T5, T6, T7, T8> commandHandler) => commandDispatcher.AddCommandHandler(commandName, commandHandler);
 
-        /// <inheritdoc cref="AddCommandHandler"/>
+        /// <inheritdoc cref="AddCommandHandler(string, Delegate)"/>
         public void AddCommandHandler<T1, T2, T3, T4, T5, T6, T7, T8, T9>(string commandName, Action<T1, T2, T3, T4, T5, T6, T7, T8, T9> commandHandler) => commandDispatcher.AddCommandHandler(commandName, commandHandler);
 
-        /// <inheritdoc cref="AddCommandHandler"/>
+        /// <inheritdoc cref="AddCommandHandler(string, Delegate)"/>
         public void AddCommandHandler<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(string commandName, Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> commandHandler) => commandDispatcher.AddCommandHandler(commandName, commandHandler);
 
-        /// <inheritdoc cref="AddCommandHandler"/>
+        /// <inheritdoc cref="AddCommandHandler(string, Delegate)"/>
         public void AddCommandHandler<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(string commandName, Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> commandHandler) => commandDispatcher.AddCommandHandler(commandName, commandHandler);
 
-        /// <inheritdoc cref="AddCommandHandler"/>
+        /// <inheritdoc cref="AddCommandHandler(string, Delegate)"/>
         public void AddCommandHandler<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>(string commandName, Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12> commandHandler) => commandDispatcher.AddCommandHandler(commandName, commandHandler);
 
-        /// <inheritdoc cref="AddCommandHandler"/>
+        /// <inheritdoc cref="AddCommandHandler(string, Delegate)"/>
         public void AddCommandHandler<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>(string commandName, Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13> commandHandler) => commandDispatcher.AddCommandHandler(commandName, commandHandler);
 
-        /// <inheritdoc cref="AddCommandHandler"/>
+        /// <inheritdoc cref="AddCommandHandler(string, Delegate)"/>
         public void AddCommandHandler<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>(string commandName, Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> commandHandler) => commandDispatcher.AddCommandHandler(commandName, commandHandler);
 
-        /// <inheritdoc cref="AddCommandHandler"/>
+        /// <inheritdoc cref="AddCommandHandler(string, Delegate)"/>
         public void AddCommandHandler<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>(string commandName, Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15> commandHandler) => commandDispatcher.AddCommandHandler(commandName, commandHandler);
 
-        /// <inheritdoc cref="AddCommandHandler"/>
+        /// <inheritdoc cref="AddCommandHandler(string, Delegate)"/>
         public void AddCommandHandler<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>(string commandName, Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16> commandHandler) => commandDispatcher.AddCommandHandler(commandName, commandHandler);
     }
 }
